c7e9291747fb8b6c53d6a4789cc8bcf2a279f248
wyanglau <wyang.lau@gmail.com>
2015-02-15 16:44:58 -0500

    CloudWatching basic function finished

---
+++ ECE1779/src/ece1779/loadBalance/CloudWatching.java
@@ -0,0 +1,99 @@
+package ece1779.loadBalance;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Vector;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchClient;
+import com.amazonaws.services.cloudwatch.model.Dimension;
+import com.amazonaws.services.cloudwatch.model.GetMetricStatisticsRequest;
+import com.amazonaws.services.cloudwatch.model.GetMetricStatisticsResult;
+import com.amazonaws.services.cloudwatch.model.ListMetricsRequest;
+import com.amazonaws.services.cloudwatch.model.ListMetricsResult;
+import com.amazonaws.services.cloudwatch.model.Metric;
+
+import ece1779.commonObjects.CloudWatcher;
+
+public class CloudWatching {
+
+	@SuppressWarnings("finally")
+	public List<CloudWatcher> getCloudWatchingStatistics(
+			BasicAWSCredentials awsCredentials) {
+		List<CloudWatcher> statistics = new ArrayList<CloudWatcher>();
+		AmazonCloudWatch cw = new AmazonCloudWatchClient(awsCredentials);
+		try {
+			ListMetricsRequest listMetricsRequest = new ListMetricsRequest();
+			listMetricsRequest.setMetricName("CPUUtilization");
+			listMetricsRequest.setNamespace("AWS/EC2");
+			ListMetricsResult result = cw.listMetrics(listMetricsRequest);
+			List<Metric> metrics = result.getMetrics();
+			for (Metric metric : metrics) {
+				statistics.add(getStatistics(metric, cw));
+			}
+
+		} catch (AmazonServiceException ase) {
+			ase.printStackTrace();
+		} catch (AmazonClientException ace) {
+			ace.printStackTrace();
+		} finally {
+			return statistics;
+		}
+	}
+
+	private CloudWatcher getStatistics(Metric metric, AmazonCloudWatch cw) {
+
+		String namespace = metric.getNamespace();
+		String metricName = metric.getMetricName();
+		List<Dimension> dimensions = metric.getDimensions();
+		GetMetricStatisticsRequest statisticsRequest = new GetMetricStatisticsRequest();
+		statisticsRequest.setNamespace(namespace);
+		statisticsRequest.setMetricName(metricName);
+		statisticsRequest.setDimensions(dimensions);
+		Date endTime = new Date();
+		Date startTime = new Date();
+		startTime.setTime(endTime.getTime() - 1200000);
+		statisticsRequest.setStartTime(startTime);
+		statisticsRequest.setEndTime(endTime);
+		statisticsRequest.setPeriod(300); // it doesn't matter what is the
+											// number here..
+		Vector<String> statistics = new Vector<String>();
+		statistics.add("Maximum");
+		statisticsRequest.setStatistics(statistics);
+		GetMetricStatisticsResult stats = cw
+				.getMetricStatistics(statisticsRequest);
+		System.out.println("Namespace = " + namespace + " Metric = "
+				+ metricName + " Dimensions = " + dimensions);
+		System.out.println("Values = " + stats.toString());
+
+		return parseStatistics(dimensions, stats);
+	}
+
+	private CloudWatcher parseStatistics(List<Dimension> dimensions,
+			GetMetricStatisticsResult stats) {
+		return new CloudWatcher(dimensions.get(0).getValue(),
+				stats.getDatapoints());
+	}
+
+	public static void main(String[] args) {
+		String accessKey = "";
+		String secretKey = "";
+		BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey,
+				secretKey);
+
+		CloudWatching cloud = new CloudWatching();
+		List<CloudWatcher> result = cloud
+				.getCloudWatchingStatistics(awsCredentials);
+
+		for (CloudWatcher w : result) {
+			System.out.println("instance id :" + w.getInstanceId());
+			System.out.println("namespace " + w.getNameSpace() + "||"
+					+ "statistic" + w.getStatistic());
+			System.out.println("datapoints:" + w.getDatapoints().toString());
+		}
+	}
+}
--- ECE1779/src/ece1779/servlets/InitializationServlet.java
+++ ECE1779/src/ece1779/servlets/InitializationServlet.java
@@ -1,8 +1,6 @@
 package ece1779.servlets;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -11,11 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.amazonaws.auth.BasicAWSCredentials;
-
-import ece1779.GlobalValues;
-import ece1779.Main;
-import ece1779.commonObjects.Images;
-import ece1779.commonObjects.User;
 
 public class InitializationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -32,25 +25,26 @@ public class InitializationServlet extends HttpServlet {
 		initAWS(config);
 		initJDBC();
 
-//		// to-do get current user
-//
-//		// This is the test data, you should get user from the session
-//		User user = new User(1, "Ryan", null);
-//		List<Images> imgs = new ArrayList<Images>();
-//		for (int i = 0; i < 5; i++) {
-//			String url = "https://s3-us-west-2.amazonaws.com/ece1779winter2015group14number1/MyObjectKey_d6310487-f3aa-4c62-81ab-79748b8975fa";
-//			List<String> keys = new ArrayList<String>();
-//			keys.add(url);
-//			keys.add(url);
-//			keys.add(url);
-//			Images img = new Images(1, keys);
-//			imgs.add(img);
-//		}
-//		user.setImgs(imgs);
-//		// Test data END
-//
-//		this.getServletContext().setAttribute(GlobalValues.USER_INIT,
-//				new Main(user));
+		// // to-do get current user
+		//
+		// // This is the test data, you should get user from the session
+		// User user = new User(1, "Ryan", null);
+		// List<Images> imgs = new ArrayList<Images>();
+		// for (int i = 0; i < 5; i++) {
+		// String url =
+		// "https://s3-us-west-2.amazonaws.com/ece1779winter2015group14number1/MyObjectKey_d6310487-f3aa-4c62-81ab-79748b8975fa";
+		// List<String> keys = new ArrayList<String>();
+		// keys.add(url);
+		// keys.add(url);
+		// keys.add(url);
+		// Images img = new Images(1, keys);
+		// imgs.add(img);
+		// }
+		// user.setImgs(imgs);
+		// // Test data END
+		//
+		// this.getServletContext().setAttribute(GlobalValues.USER_INIT,
+		// new Main(user));
 
 	}
 
